9376f7bb1631fe821d6c2ecf8e630efd3803f20f
srtinoco <37519475+srtinoco@users.noreply.github.com>
2018-03-27 20:43:02 +0100

    Major character/map development
    Created Player movement, camera movement and flaslight.
    Imported the church model.

---
+++ Nygma/Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour {
+    public Transform playerBody;
+    public Transform flashlight;
+    public float mouseSensitivity;
+    public bool cursor;
+    float xAxisClamp = 0.0f;
+
+    void Awake()
+    {
+        Cursor.lockState = !cursor ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotFlash = flashlight.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotFlash.x -= rotAmountY;
+        targetRotFlash.y += rotAmountX;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        print(mouseY);
+
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+        flashlight.rotation = Quaternion.Euler(targetRotFlash);
+
+
+    }
+}
---
+++ Nygma/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    CharacterController charControl;
+    private float verticalVelocity;
+    private float gravity = 9.8f;
+    public float jumpForce;
+    public float walkSpeed;
+
+    
+
+    void Awake()
+    {
+        charControl = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 moveDirSide = transform.right * horiz * walkSpeed;
+        Vector3 moveDirForward = transform.forward * vert * walkSpeed;
+
+
+        charControl.SimpleMove(moveDirSide);
+        charControl.SimpleMove(moveDirForward);
+        
+    }
+}
